ab73c6d1e0df6c327be5cc0167055314cbd1bf35
Gustavo Rodrigues <87579597+duguzin@users.noreply.github.com>
2023-11-23 18:01:39 -0300

    Add files via upload

--- Js/script.js
+++ Js/script.js
@@ -198,4 +198,12 @@ function filtrarPc() {
 
 }
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
 
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
